077db7aa86180202ad65767a6d200d553f1322cd
Kirilo <kirill.korovyakovskiy@gmail.com>
2024-06-24 10:08:10 +0300

    Homework

--- HmWrk1/ok2/ok2/Form1.Designer.cs
+++ HmWrk1/ok2/ok2/Form1.Designer.cs
@@ -36,17 +36,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(352, 260);
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 20.23F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(220, 173);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(504, 58);
+            this.button1.Size = new System.Drawing.Size(466, 63);
             this.button1.TabIndex = 0;
-            this.button1.Text = "GET FREE ROBUX NOWWWW";
+            this.button1.Text = "Encrypt";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -54,7 +55,7 @@
             // 
             this.textBox1.AccessibleDescription = "";
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(175, 92);
+            this.textBox1.Location = new System.Drawing.Point(91, 21);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(1135, 45);
             this.textBox1.TabIndex = 1;
@@ -62,7 +63,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(175, 174);
+            this.textBox2.Location = new System.Drawing.Point(91, 91);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(1135, 45);
             this.textBox2.TabIndex = 2;
@@ -71,16 +72,16 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(175, 412);
+            this.textBox3.Location = new System.Drawing.Point(107, 277);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(1135, 45);
+            this.textBox3.Size = new System.Drawing.Size(734, 45);
             this.textBox3.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label1.Location = new System.Drawing.Point(94, 100);
+            this.label1.Location = new System.Drawing.Point(10, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 31);
             this.label1.TabIndex = 4;
@@ -90,17 +91,17 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label2.Location = new System.Drawing.Point(56, 182);
+            this.label2.Location = new System.Drawing.Point(3, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 31);
+            this.label2.Size = new System.Drawing.Size(69, 31);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Vidstup:";
+            this.label2.Text = "Key:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label3.Location = new System.Drawing.Point(69, 420);
+            this.label3.Location = new System.Drawing.Point(1, 285);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 31);
             this.label3.TabIndex = 6;
@@ -110,7 +111,7 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.button2.Font = new System.Drawing.Font("Comic Sans MS", 23.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(707, 335);
+            this.button2.Location = new System.Drawing.Point(469, 423);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(454, 58);
             this.button2.TabIndex = 7;
@@ -118,12 +119,25 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 20.23F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(692, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(466, 63);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Olive;
             this.ClientSize = new System.Drawing.Size(1420, 566);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -151,6 +165,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
---
+++ HmWrk1/ok2/ok2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ok2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public char[] bukovka = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        // encrypt
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text.ToLower();
+            int key = Convert.ToInt32(textBox2.Text);
+            if (key != 0)
+            {
+                if (textBox1.Text.Trim().Length != 0)
+                {
+                    textBox3.Clear();
+                    foreach (char c in str)
+                    {
+                        if (c != ' ')
+                        {
+                            for (int i = 0; i < bukovka.Length; i++)
+                            {
+                                if (c == bukovka[i])
+                                {
+                                    int j = (i + key) % 26;
+                                    textBox3.Text += bukovka[j];
+                                }
+                            }
+                        }
+                        else
+                        {
+                            textBox3.Text += ' ';
+                        }
+                    }
+                }
+            }
+        }
+
+        //decrypt
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text.ToLower();
+            int key = Convert.ToInt32(textBox2.Text);
+            if (key != 0)
+            {
+                if (textBox1.Text.Trim().Length != 0)
+                {
+                    textBox3.Clear();
+                    foreach (char c in str)
+                    {
+                        if (c != ' ')
+                        {
+                            for (int i = 0; i < bukovka.Length; i++)
+                            {
+                                if (c == bukovka[i])
+                                {
+                                    int j = i - key;
+                                    if (j >= 0)
+                                    {
+                                        textBox3.Text += bukovka[j];
+                                    }
+                                    else
+                                    {
+                                        j = -j;
+                                        textBox3.Text += bukovka[j];
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            textBox3.Text += ' ';
+                        }
+                    }
+                }
+            }
+        }
+
+        //clear
+        private void button2_Click(object sender, EventArgs e)
+        {                     
+            textBox1.Text = "";
+            textBox2.Text = "3";
+            textBox3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
